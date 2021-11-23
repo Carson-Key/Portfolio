ece76cfb290cb5ecdc887df063bd30ef0c75f639
Carson-Key <keyc@oregonstate.edu>
2021-11-22 16:56:14 -0800

    Add basic nav elements

--- src/ui/Navigation.js
+++ src/ui/Navigation.js
@@ -2,9 +2,11 @@ const App = () => {
 	return (
 		<nav>
 			<ul>
-                <li>nav 1</li>
-                <li>nav 2</li>
-                <li>nav 3</li>
+                <li>Projects</li>
+                <li>Work</li>
+                <li>Eduction</li>
+                <li>Resume</li>
+                <li>About</li>
             </ul>
 		</nav>
 	)
