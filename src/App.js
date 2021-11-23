a709ddf060cf49857cf8f8279e033ca124e13e82
Carson-Key <keyc@oregonstate.edu>
2021-11-22 16:51:28 -0800

    Add Navigation component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navigation from './ui/Navigation'
+
 const App = () => {
 	return (
 		<main>
-			<p>Hello World</p>
+			<Navigation />
 		</main>
 	)
 }
---
+++ src/ui/Navigation.js
@@ -0,0 +1,13 @@
+const App = () => {
+	return (
+		<nav>
+			<ul>
+                <li>nav 1</li>
+                <li>nav 2</li>
+                <li>nav 3</li>
+            </ul>
+		</nav>
+	)
+}
+
+export default App
