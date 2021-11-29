dc7c2c60e601811b821722efa5d3a8a1fcea5bab
Carson-Key <keyc@oregonstate.edu>
2021-11-29 11:03:33 -0800

    Add drawer menu css Refers #1

--- src/ui/Navigation.js
+++ src/ui/Navigation.js
@@ -4,19 +4,19 @@ import Logo512 from '../assets/Logo-512.png'
 const Navigation = () => {
 	return (
 		<nav className="flex w-screen h-14 bg-secondary text-white">
-            <MdMenu className="absolute inset-y-0 left-0 h-full text-4xl ml-2"/>
+            <MdMenu className="absolute inset-y-0 left-0 h-14 text-4xl ml-2"/>
             <div className="flex justify-center my-auto mx-auto">
                 <h1 className="my-auto text-lg font-helvetica">Carson</h1>
                 <img src={Logo512} className="h-10 ml-1" alt="" />
                 <h1 className="my-auto text-lg font-helvetica ml-1">Key</h1>
             </div>
-			{/* <ul>
+			<ul className="absolute top-14 left-0 w-screen h-screen bg-secondary bg-opacity-90">
                 <li>Projects</li>
                 <li>Work</li>
                 <li>Eduction</li>
                 <li>Resume</li>
                 <li>About</li>
-            </ul> */}
+            </ul>
 		</nav>
 	)
 }
