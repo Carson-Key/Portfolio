06365b46266922f012438a5f0ad6510965e6cd4c
Carson-Key <keyc@oregonstate.edu>
2021-11-28 21:13:57 -0800

    Add mobile css for navigation Refers #1

--- src/ui/Navigation.js
+++ src/ui/Navigation.js
@@ -1,7 +1,14 @@
+import { MdMenu } from 'react-icons/md'
+import Logo512 from '../assets/Logo-512.png'
+
 const Navigation = () => {
 	return (
-		<nav className="bg-secondary">
-            <h1 className="text-white">Carson Key</h1>
+		<nav className="flex w-screen h-14 bg-secondary text-white">
+            <MdMenu className="absolute inset-y-0 left-0 h-full text-2xl ml-2"/>
+            <div className="flex justify-center col-span-2 w-2/3 my-auto mx-auto">
+                <img src={Logo512} className="h-10" alt="" />
+                <h1 className="my-auto text-xl ml-1">Carson Key</h1>
+            </div>
 			{/* <ul>
                 <li>Projects</li>
                 <li>Work</li>
