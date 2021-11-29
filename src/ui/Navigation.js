f1344400fe552d9b4c59b6cf49ade3233d89f9c1
Carson-Key <keyc@oregonstate.edu>
2021-11-28 21:16:53 -0800

    Increase size of hamburger menu Refers #1

--- src/ui/Navigation.js
+++ src/ui/Navigation.js
@@ -4,10 +4,10 @@ import Logo512 from '../assets/Logo-512.png'
 const Navigation = () => {
 	return (
 		<nav className="flex w-screen h-14 bg-secondary text-white">
-            <MdMenu className="absolute inset-y-0 left-0 h-full text-2xl ml-2"/>
+            <MdMenu className="absolute inset-y-0 left-0 h-full text-4xl ml-2"/>
             <div className="flex justify-center col-span-2 w-2/3 my-auto mx-auto">
                 <img src={Logo512} className="h-10" alt="" />
-                <h1 className="my-auto text-xl ml-1">Carson Key</h1>
+                <h1 className="my-auto text-lg ml-1">Carson Key</h1>
             </div>
 			{/* <ul>
                 <li>Projects</li>
