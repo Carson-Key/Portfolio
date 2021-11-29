ec86602a07aa0181b75140880a6759ff9890accd
Carson-Key <keyc@oregonstate.edu>
2021-11-29 11:23:51 -0800

    Add z index to drawer menu Refers #1

--- src/ui/Navigation.js
+++ src/ui/Navigation.js
@@ -3,14 +3,16 @@ import Logo512 from '../assets/Logo-512.png'
 
 const Navigation = () => {
 	return (
-		<nav className="flex w-screen h-14 bg-secondary text-white">
-            <MdMenu className="absolute inset-y-0 left-0 h-14 text-4xl ml-2"/>
-            <div className="flex justify-center my-auto mx-auto">
-                <h1 className="my-auto text-lg font-helvetica">Carson</h1>
-                <img src={Logo512} className="h-10 ml-1" alt="" />
-                <h1 className="my-auto text-lg font-helvetica ml-1">Key</h1>
+		<nav>
+            <div className="flex relative w-screen h-14 z-40 bg-secondary text-white">
+                <MdMenu className="absolute inset-y-0 left-0 h-14 text-4xl ml-2"/>
+                <div className="flex justify-center my-auto mx-auto">
+                    <h1 className="my-auto text-lg font-helvetica">Carson</h1>
+                    <img src={Logo512} className="h-10 ml-1" alt="" />
+                    <h1 className="my-auto text-lg font-helvetica ml-1">Key</h1>
+                </div>
             </div>
-			<ul className="absolute top-14 left-0 w-screen h-screen bg-secondary bg-opacity-90">
+			<ul className="absolute top-0 left-0 z-0 w-screen h-screen bg-secondary bg-opacity-90 pt-14">
                 <li>Projects</li>
                 <li>Work</li>
                 <li>Eduction</li>
