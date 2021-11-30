c2f0cc786ea50807c9e06254fc25b4a842750a7a
Carson-Key <keyc@oregonstate.edu>
2021-11-29 17:47:03 -0800

    Increase size of nav items Refers #1

--- src/ui/Navigation.js
+++ src/ui/Navigation.js
@@ -22,7 +22,7 @@ const Navigation = () => {
 			<ul className={
                 "absolute top-0 left-0 z-0" + 
                 " transform ease-in-out transition-all duration-500 translate-x-0" +
-                " w-screen h-screen divide-y px-2 pt-14" +
+                " w-screen h-screen divide-y px-2 pt-14 text-xl" +
                 " bg-drawer-menu bg-contain bg-no-repeat bg-right-bottom bg-secondary bg-opacity-90" + 
                 (drawerMenuIsOpen ? " translate-x-0" : " -translate-x-full")
             }>
