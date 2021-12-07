b7a24a2819f7b0488ebc2724260f013d87e51968
Carson-Key <keyc@oregonstate.edu>
2021-12-07 14:19:47 -0800

    Increase padding of mobile nav links

--- src/ui/Navigation.js
+++ src/ui/Navigation.js
@@ -9,24 +9,24 @@ const Navigation = (props) => {
                 // Mobile
                 "absolute top-0 left-0 -z-10" + 
                 " transform ease-in-out transition-all duration-500 translate-x-0" +
-                " w-screen h-screen divide-y px-2 pt-14 text-xl" +
+                " w-screen h-screen divide-y px-2 pt-14 text-2xl" +
                 " bg-drawer-menu bg-contain bg-no-repeat bg-right-bottom bg-secondary bg-opacity-90" + 
                 (drawerMenuIsOpen ? " translate-x-0" : " -translate-x-full") +
                 // Small Computer
                 " lg:static lg:flex" +
                 " lg:transform-none" +
-                " lg:w-auto lg:h-auto lg:divide-y-0 lg:divide-x lg:mr-0 lg:ml-8 lg:p-0 lg:text-2xl" + 
+                " lg:w-auto lg:h-auto lg:divide-y-0 lg:divide-x lg:mr-0 lg:ml-8 lg:p-0 lg:text-3xl" + 
                 " lg:bg-none lg:bg-transparent" +
                 // Large Computer
-                " xl:text-3xl" +
+                " xl:text-4xl" +
                 // Extremely Large Computer
-                " 2xl:text-4xl"
+                " 2xl:text-5xl"
             }>
-                <li><Link to="/Projects"><p className="px-3 lg:px-4" onClick={closeDrawerMenu}>Projects</p></Link></li>
-                <li><Link to="/Work"><p className="px-3 lg:px-4" onClick={closeDrawerMenu}>Work</p></Link></li>
-                <li><Link to="/Education"><p className="px-3 lg:px-4" onClick={closeDrawerMenu}>Education</p></Link></li>
-                <li><Link to="/Resume"><p className="px-3 lg:px-4" onClick={closeDrawerMenu}>Resume</p></Link></li>
-                <li><Link to="/About"><p className="px-3 lg:px-4" onClick={closeDrawerMenu}>About</p></Link></li>
+                <li className="py-2 lg:py-0"><Link to="/Projects"><p className="px-3 lg:px-4" onClick={closeDrawerMenu}>Projects</p></Link></li>
+                <li className="py-2 lg:py-0"><Link to="/Work"><p className="px-3 lg:px-4" onClick={closeDrawerMenu}>Work</p></Link></li>
+                <li className="py-2 lg:py-0"><Link to="/Education"><p className="px-3 lg:px-4" onClick={closeDrawerMenu}>Education</p></Link></li>
+                <li className="py-2 lg:py-0"><Link to="/Resume"><p className="px-3 lg:px-4" onClick={closeDrawerMenu}>Resume</p></Link></li>
+                <li className="py-2 lg:py-0"><Link to="/About"><p className="px-3 lg:px-4" onClick={closeDrawerMenu}>About</p></Link></li>
             </ul>
 		</nav>
 	)
