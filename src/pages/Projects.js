de0d77a39e4ccf884117e3283288fcd142855377
Carson-Key <keyc@oregonstate.edu>
2021-12-18 11:11:53 -0800

    Add border to basic project card Refers #9

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,7 +4,7 @@ import Container from '../components/Container'
 const Projects = () => {
     return (
 		<Container className="flex flex-wrap justify-evenly">
-			<section className={"m-4 w-72 h-72 rounded-xl"}>
+			<section className={"m-4 w-72 h-72 rounded-2xl border-2 border-secondary"}>
 				Project!
 			</section>
 		</Container>
