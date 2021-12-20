e7fac4b812c3d097b21c9e72bb8209979233c0dc
Carson-Key <keyc@oregonstate.edu>
2021-12-20 13:35:05 -0800

    Add h minimum to Card component Refers #9

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ const Card = (props) => {
     const { children } = props
 
     return (
-        <section className={"m-4 w-72 h-72 rounded-2xl border-2 border-secondary"}>
+        <section className={"m-4 w-72 h-min-fit rounded-2xl border-2 border-secondary"}>
             {children}
         </section>
 	)
