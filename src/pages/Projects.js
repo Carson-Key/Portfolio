76b2be17c89c54938bbd938dd61bd841272dbaa2
Carson-Key <keyc@oregonstate.edu>
2021-12-29 15:32:44 -0800

    Add more realistic card content for creating css

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,23 +2,22 @@
 import Container from '../components/Container'
 import ProjectCard from '../ui/ProjectCard'
 // Assets
-import BabyYoda from '../assets/baby_yoda.jpeg'
+import Logo from '../assets/Logo-512.png'
 
 const Projects = () => {
     return (
 		<Container className="flex flex-wrap justify-evenly">
 			<ProjectCard
-				title="Test Card"
+				title="This Very Portfolio!"
 				image={
 					{
-						src: BabyYoda,
-						alt: "baby yoda"
+						src: Logo,
+						alt: "Brass colored Node.js hexagon logo with brass colored React logo inside, which has the nucleus replaced with a brass key"
 					}
 				}
-				description="This is a test project"
-				projectLink="google.com"
-				githubLink="github.com"
-				work="brokengm.com"
+				description="I wanted to make a portfolio that would both display my accomplishments and experiences, but to also act as one. This portfolio website was created using React.js for frontend, Tailwind for css, GraphQL for backend, and AWS Amplify as a serverless framework. In a design perspective this is overkill, a portfolio website is as static of a website as they come. HTML and Tailwind would have done the job at a far better labor cost. But I decided that the extra costs in design, testing, and implemtation would not outweigh the benefits of having a far more presentable portfolio with a better display of my skills."
+				projectLink="/"
+				githubLink="https://github.com/Carson-Key/Portfolio"
 			 />
 		</Container>
 	)
