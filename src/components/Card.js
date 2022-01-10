eccea4bcbb4dc9f6db848b408ded4a0fd0a9cf64
Carson-Key <keyc@oregonstate.edu>
2022-01-10 15:51:17 -0800

    Add static height to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ const Card = (props) => {
     const { children, className } = props
 
     return (
-        <section className={"m-4 w-80 h-96 rounded-2xl border-2 border-secondary " + className}>
+        <section className={"m-2 w-80 h-96 rounded-2xl border-2 border-secondary " + className}>
             {children}
         </section>
 	)
