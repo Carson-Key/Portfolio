605f5ba13aa755cea623f3827d877fc3eebbc926
Carson-Key <keyc@oregonstate.edu>
2022-01-10 12:33:50 -0800

    Change Card height to 96

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ const Card = (props) => {
     const { children, className } = props
 
     return (
-        <section className={className + " m-4 w-80 h-min-fit rounded-2xl border-2 border-secondary"}>
+        <section className={className + " m-4 w-80 h-96 rounded-2xl border-2 border-secondary"}>
             {children}
         </section>
 	)
