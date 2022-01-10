b3919230774bf86b70b0aaaafdd35eaa49d44722
Carson-Key <keyc@oregonstate.edu>
2022-01-10 12:24:32 -0800

    Add className prop to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,8 @@
 const Card = (props) => {
-    const { children } = props
+    const { children, className } = props
 
     return (
-        <section className={"m-4 w-72 h-min-fit rounded-2xl border-2 border-secondary"}>
+        <section className={className + " m-4 w-72 h-min-fit rounded-2xl border-2 border-secondary"}>
             {children}
         </section>
 	)
