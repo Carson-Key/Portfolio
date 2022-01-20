1bf28559213ccb01cec29fbdd0fa52ecbc272e1a
Carson-Key <keyc@oregonstate.edu>
2022-01-20 12:21:32 -0800

    Remove Width from footer elements Refers #13

--- src/ui/WorkCard/WorkCardFooter.js
+++ src/ui/WorkCard/WorkCardFooter.js
@@ -10,12 +10,12 @@ const WorkCardFooter = (props) => {
     return (
         <div className="flex justify-between h-10 mx-2 mb-1 mt-2 transition-all duration-500 ease-out">
             <ConditionalRender condition={years}>
-                <div className="w-2/3 text-sm grid grid-cols-2">
+                <div className="text-sm grid grid-cols-2">
                     <p>From</p><p>To</p>
 				    <p>{years.from}</p><p>{years.to}</p>
                 </div>
             </ConditionalRender>
-            <div className="flex justify-end w-1/3">
+            <div className="flex justify-end">
                 <button onClick={toggleExpanded} className="text-lg text-primary underline">{expandButtonText}</button> 
             </div>
         </div>
