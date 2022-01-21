28c6697cef65d98e3ac2cec26c8c18976f7d1168
Carson-Key <keyc@oregonstate.edu>
2022-01-21 14:37:05 -0800

    Add basic footer layout Refers #33

---
+++ src/ui/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<footer className="h-12 bg-secondary text-white mt-10 relative inset-x-0 bottom-0">
+            <p>test</p>
+		</footer>
+	)
+}
+
+export default Footer
