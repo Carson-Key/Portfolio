70570193f8274b4d352106561c64a54b7f64845b
Carson-Key <keyc@oregonstate.edu>
2022-01-21 14:09:56 -0800

    Add basic layout for about page Refers #26

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,15 @@
 // Components
 import Container from '../components/Container'
+// Assests
+import CarsonPicture from '../assets/Profile-Picture.png'
+import BendiPicture from '../assets/Profile-Kitty.png'
 
 const About = () => {
     return (
 		<Container>
-			<p>About</p>
+			<img className="float-left w-1/2 my-2 mx-2" src={CarsonPicture}/>
+			<h3 className="font-bold">About Me</h3>
+			<p><img className="float-right w-1/2 my-2 mx-2" src={BendiPicture}/></p>
 		</Container>
 	)
 }
