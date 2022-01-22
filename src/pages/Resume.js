a9862f9c2b9c3bc3ec8644847fbe2e7ef2af1cf5
Carson-Key <keyc@oregonstate.edu>
2022-01-22 11:11:06 -0800

    Update resume pdf link to be from dropbox

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -8,11 +8,11 @@ const Resume = () => {
 		<Container className="overflow-x-scroll w-screen">
 			<center>
 				<Document
-					file={process.env.PUBLIC_URL + "/Resume.pdf"}
+					file="https://www.dropbox.com/s/3xjp930yfcby5n7/Resume.pdf?raw=1"
 				>
 					<Page height={window.height} pageNumber={1} />
 				</Document>
-				<a className="w-auto mx-2 bg-primary rounded text-white py-1 px-2" href={process.env.PUBLIC_URL + "/Resume.pdf"}>Download</a>
+				<a className="w-auto mx-2 bg-primary rounded text-white py-1 px-2" href={"https://www.dropbox.com/s/3xjp930yfcby5n7/Resume.pdf?raw=1"}>Download</a>
 			</center>
 		</Container>
 	)
